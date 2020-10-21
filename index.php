c4dea3b07a3dc84af8afa23183bdd374a46e997c
hohoho <gramafon@yandex.ru>
2020-10-21 03:45:19 +0300

    Клубная и обычная цена цена меняется от выбранного расчета стоимости задача  расчет стоимости в зависиомотси от кол-ва товара

--- index.php
+++ index.php
@@ -169,6 +169,7 @@ $(".unit--select").click(function() {
     let mainCondition = this.classList.contains("unit--active")
     if (mainCondition === false)   {
         let conditionPriceGold = this.parentNode.parentNode.nextElementSibling.children[1].classList.contains("price--gold")
+        let conditionPriceRetail = this.parentNode.parentNode.nextElementSibling.nextElementSibling.children[0].classList.contains("price")
         let currentId = this.parentNode.parentNode.nextElementSibling.children[1].id
         console.log(currentId)
         this.className += " unit--active"     
@@ -182,6 +183,9 @@ $(".unit--select").click(function() {
             if (currentId == getJson[key].productId) {
                 var currentPriceGold = getJson[key].priceGold
                 var currentPriceGoldAlt = getJson[key].priceGoldAlt
+                
+                var currentPrice = getJson[key].priceRetail
+                var currentPriceAlt = getJson[key].priceRetailAlt
             }                             
         }        
         if (conditionPriceGold === false) {
@@ -191,7 +195,15 @@ $(".unit--select").click(function() {
         if (conditionPriceGold === true) {
             this.parentNode.parentNode.nextElementSibling.children[1].classList.remove("price--gold") 
             this.parentNode.parentNode.nextElementSibling.children[1].innerHTML = currentPriceGoldAlt
-        }        
+        } 
+        if (conditionPriceRetail === false) {
+            this.parentNode.parentNode.nextElementSibling.nextElementSibling.children[0].className += " price"
+            this.parentNode.parentNode.nextElementSibling.nextElementSibling.children[0].innerHTML = currentPrice
+        } 
+        if (conditionPriceRetail === true) {
+            this.parentNode.parentNode.nextElementSibling.nextElementSibling.children[0].classList.remove("price") 
+            this.parentNode.parentNode.nextElementSibling.nextElementSibling.children[0].innerHTML = currentPriceAlt
+        }         
     }          
 })
 </script>
